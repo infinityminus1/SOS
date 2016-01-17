fedc2bb00d8622e548ba88a353f4444a78094d2e
Kurt Lang <kurtalang@gmail.com>
2016-01-16 17:09:11 -0800

    connect to mongodb instance on heroic

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,9 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/test',
+  mongoDBUrl: process.env.MONGODB_URL ||
+              process.env.MONGODB_URI ||
+              'mongodb://localhost:27017/test',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
